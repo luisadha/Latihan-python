fed383ae5f00e0d0dca8ce6e51d27d958d8ffcdd
Luis Adha <40796508+luisadha@users.noreply.github.com>
2024-05-01 22:07:23 +0700

    Create operasi_aritmatika.py

---
+++ operasi_aritmatika.py
@@ -0,0 +1,13 @@
+def operasi(operator, angka1, angka2):
+    if operator == "+":
+        return angka1 + angka2
+    elif operator == "-":
+        return angka1 - angka2
+    elif operator == "*":
+        return angka1 * angka2
+    else:
+        return "hehehe"
+
+print(operasi("+", 10, 10));
+
+
